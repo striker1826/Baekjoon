850d01b89dffaff136c275e49d052a1ad630b0ec
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-12-28 21:45:16 +0900

    feat: A+B 22-12-28 21:45

--- 01. 입출력과 사칙연산/02. A+B.js
+++ 01. 입출력과 사칙연산/02. A+B.js
@@ -1,9 +1,19 @@
 /*
 1 2
 */
-
 const fs = require("fs");
-let input = fs.readFileSync("./input.txt").toString();
-input = input.split("\n");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
 
-console.log(input);
+const a = input[0];
+const b = input[1];
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+add(a, b);
