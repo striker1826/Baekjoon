092d12239a2630a14d83216d34169d80c0500a7d
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2023-01-04 23:25:42 +0900

    feat: 영수증 23-01-04 23:25

--- 03.반복문/02.A+B - 3.js
+++ 03.반복문/02.A+B - 3.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
-const interations = Number("5");
-const array = ["5", "1 1", "2 3", "3 4", "9 8", "5 2"];
+const interations = Number(input[0]);
 
 for (let i = 1; i <= interations; i++) {
-  const [a, b] = array[i].split(" ").map(Number);
-  console.log(a, b);
-  console.log("--------");
+  const [a, b] = input[i].split(" ").map(Number);
+  console.log(a + b);
 }
